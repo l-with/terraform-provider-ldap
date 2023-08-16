83ffc19951f96557886c2eadded8a8b524b6982e
Lars With <lars@with.de>
2023-08-16 12:43:03 +0200

    use constants

--- ldap/data_source_entry.go
+++ ldap/data_source_entry.go
@@ -8,49 +8,56 @@ import (
 	"github.com/l-with/terraform-provider-ldap/client"
 )
 
+const attributeNameOu = "ou"
+const attributeNameFilter = "filter"
+const attributeNameIgnoreAttributes = "ignore_attributes"
+const attributeNameIgnoreAttributePatterns = "ignore_attribute_patterns"
+const attributeNameBase64EncodeAttributes = "base64encode_attributes"
+const attributeNameBase64EncodeAttributePatterns = "base64encode_attribute_patterns"
+
 func dataSourceLDAPEntry() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceLDAPEntryRead,
 		Schema: map[string]*schema.Schema{
-			"dn": {
+			attributeNameDn: {
 				Description: "DN of the LDAP entry",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
-			"ou": {
+			attributeNameOu: {
 				Description: "OU where LDAP entry will be searched",
 				Type:        schema.TypeString,
 				Required:    true,
 			},
-			"filter": {
+			attributeNameFilter: {
 				Description: "filter for selecting the LDAP entry",
 				Type:        schema.TypeString,
 				Required:    true,
 			},
-			"data_json": {
+			attributeNameDataJson: {
 				Description: "JSON-encoded string that is read as the values of the attributes of the entry (s. https://pkg.go.dev/github.com/go-ldap/ldap/v3#EntryAttribute)",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
-			"ignore_attributes": {
+			attributeNameIgnoreAttributes: {
 				Description: "list of attributes to ignore",
 				Type:        schema.TypeList,
 				Optional:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
-			"ignore_attribute_patterns": {
+			attributeNameIgnoreAttributePatterns: {
 				Description: "list of attribute patterns to ignore",
 				Type:        schema.TypeList,
 				Optional:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
-			"base64encode_attributes": {
+			attributeNameBase64EncodeAttributes: {
 				Description: "list of attributes to be encoded to base64",
 				Type:        schema.TypeList,
 				Optional:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
-			"base64encode_attribute_patterns": {
+			attributeNameBase64EncodeAttributePatterns: {
 				Description: "list of attribute patterns to be encoded to base64",
 				Type:        schema.TypeList,
 				Optional:    true,
@@ -63,22 +70,22 @@ func dataSourceLDAPEntry() *schema.Resource {
 func dataSourceLDAPEntryRead(_ context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	cl := m.(*client.Client)
 
-	ou := d.Get("ou").(string)
-	filter := d.Get("filter").(string)
+	ou := d.Get(attributeNameOu).(string)
+	filter := d.Get(attributeNameFilter).(string)
 	var ignore_attributes []string
-	for _, ignore_attribute := range d.Get("ignore_attributes").([]interface{}) {
+	for _, ignore_attribute := range d.Get(attributeNameIgnoreAttributes).([]interface{}) {
 		ignore_attributes = append(ignore_attributes, ignore_attribute.(string))
 	}
 	var ignore_attribute_patterns []string
-	for _, ignore_attribute_pattern := range d.Get("ignore_attribute_patterns").([]interface{}) {
+	for _, ignore_attribute_pattern := range d.Get(attributeNameIgnoreAttributePatterns).([]interface{}) {
 		ignore_attribute_patterns = append(ignore_attribute_patterns, ignore_attribute_pattern.(string))
 	}
 	var base64encode_attributes []string
-	for _, base64encode_attribute := range d.Get("base64encode_attributes").([]interface{}) {
+	for _, base64encode_attribute := range d.Get(attributeNameBase64EncodeAttributes).([]interface{}) {
 		base64encode_attributes = append(base64encode_attributes, base64encode_attribute.(string))
 	}
 	var base64encode_attribute_patterns []string
-	for _, base64encode_attribute_pattern := range d.Get("base64encode_attribute_patterns").([]interface{}) {
+	for _, base64encode_attribute_pattern := range d.Get(attributeNameBase64EncodeAttributePatterns).([]interface{}) {
 		base64encode_attribute_patterns = append(base64encode_attribute_patterns, base64encode_attribute_pattern.(string))
 	}
 
@@ -91,7 +98,7 @@ func dataSourceLDAPEntryRead(_ context.Context, d *schema.ResourceData, m interf
 	id := ldapEntry.Dn
 	d.SetId(id)
 
-	err = d.Set("dn", id)
+	err = d.Set(attributeNameDn, id)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -101,7 +108,7 @@ func dataSourceLDAPEntryRead(_ context.Context, d *schema.ResourceData, m interf
 		return diag.Errorf("error marshaling JSON for %q: %s", id, err)
 	}
 
-	if err := d.Set("data_json", string(jsonData)); err != nil {
+	if err := d.Set(attributeNameDataJson, string(jsonData)); err != nil {
 		return diag.FromErr(err)
 	}
 
