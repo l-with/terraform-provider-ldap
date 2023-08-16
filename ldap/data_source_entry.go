905d6bdfa12d41e0713b80bfb974733d7552254e
Lars With <lars@with.de>
2023-08-16 12:32:22 +0200

    tidy up

--- ldap/data_source_entry.go
+++ ldap/data_source_entry.go
@@ -3,7 +3,6 @@ package ldap
 import (
 	"context"
 	"encoding/json"
-	"github.com/go-ldap/ldap/v3"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/l-with/terraform-provider-ldap/client"
@@ -62,10 +61,6 @@ func dataSourceLDAPEntry() *schema.Resource {
 }
 
 func dataSourceLDAPEntryRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return resourceLDAPEntryRead(context.WithValue(ctx, CallerTypeKey, DatasourceCaller), d, m)
-}
-
-func resourceLDAPEntryRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	cl := m.(*client.Client)
 
 	ou := d.Get("ou").(string)
@@ -90,19 +85,6 @@ func resourceLDAPEntryRead(ctx context.Context, d *schema.ResourceData, m interf
 	ldapEntry, err := cl.ReadEntryByFilter(ou, "("+filter+")", ignore_attributes, ignore_attribute_patterns, base64encode_attributes, base64encode_attribute_patterns)
 
 	if err != nil {
-		if err.(*ldap.Error).ResultCode == ldap.LDAPResultNoSuchObject {
-			// Object doesn't exist
-
-			// If Read is called from a datasource, return an error
-			if ctx.Value(CallerTypeKey) == DatasourceCaller {
-				return diag.FromErr(err)
-			}
-
-			// If not a call from datasource, remove the resource from the state
-			// and cleanly return
-			d.SetId("")
-			return nil
-		}
 		return diag.FromErr(err)
 	}
 
