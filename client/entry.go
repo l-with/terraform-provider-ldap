e08aa17a8814f19b9759d9d11bccc6e5fa0c2830
Lars With <lars@with.de>
2023-08-18 17:11:34 +0200

    remove debug log

--- client/entry.go
+++ client/entry.go
@@ -152,8 +152,6 @@ func (c *Client) ReadEntriesByFilter(ou string, filter string, ignore_attributes
 }
 
 func (c *Client) ReadEntryByDN(dn string) (ldapEntry *LdapEntry, err error) {
-	log.Printf("[DEBUG] ReadEntryByDN dn: %s", dn)
-
 	req := ldap.NewSearchRequest(
 		dn,
 		ldap.ScopeBaseObject,
